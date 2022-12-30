ab9bc252ccabd655ee110870f777f395cee50a18
QiutingWang <96046280+QiutingWang@users.noreply.github.com>
2022-12-30 17:03:16 +0800

    Update Character Array.cpp

--- Character Array.cpp
+++ Character Array.cpp
@@ -157,3 +157,148 @@ int main(){
 // Your name is Doris Wang
 // Reverse Character Array: gnaW siroD
 // Length: 10
+
+
+///////////////////////////
+/////Inbuilt Functions/////
+#include<cstring>         //manipulate strings and arrays
+/////1. strlen()
+int main(){
+  char string1[100];
+  cin>>string1;
+  int len=strlen(string1);  //strlen(string) returns the length of the given C-string.
+  cout<<"Length: "<<len<<endl;
+  return 0;
+}
+// return:
+// Hello
+// Length: 5
+
+/////2. Compare two strings character by character: strcmp(str1, str2). 
+// If the strings are equal, the function returns 0.  >0: str1>str2.  <0: str1<str2.
+int main(){
+  char string1[100];
+  char string2[100];
+  cin>>string1>>string2;
+
+  if (strcmp(string1, string2)==0){
+    cout<<"equal"<<endl;
+  }else{
+    cout<<"not equal"<<endl;
+  }
+
+  return 0;
+}
+// return:
+// Hello Hello
+// equal
+
+// write the code about strcmp() function
+bool mystrcmp(char str1[], char str2[]){
+  if (strlen(str1) != strlen(str2)) return false;
+
+  for (int i=0; i<strlen(str1); i++){
+    if (str1[i] != str2[i]) return false;
+  }
+  return true;
+}
+
+/////3. Copy one string and do another string: strcpy(destinationString, sourceString)
+int main(){
+  char str1[100]="Doris";
+  char str2[100]="Wang";
+  
+  cout<<"Before Copying"<<endl;
+  cout<<"str1 "<<str1<<endl;
+  cout<<"str2 "<<str2<<endl;
+
+  strcpy(str1, str2); //copy the content of string2 to string1, and replace the original content in string1
+
+  cout<<"After Copying"<<endl;
+  cout<<"str1 "<<str1<<endl;
+  cout<<"str2 "<<str2<<endl;
+
+}
+// return:
+// Before Copying
+// str1 Doris    //Doris \0
+// str2 Wang
+// After Copying
+// str1 Wang     // Wang \0 \0, it also copies the null character.
+// str2 Wang     // string2 is not affected.
+
+/////4. Copy the first n characters: strncpy(destinationString, sourceString, n); n: how many characters you want to copy
+int main(){
+  char str1[100]="Happy";
+  char str2[100]="NewYear";
+  
+  cout<<"Before Copying"<<endl;
+  cout<<"str1 "<<str1<<endl;
+  cout<<"str2 "<<str2<<endl;
+
+  strncpy(str1, str2, 4); 
+
+  cout<<"After Copying"<<endl;
+  cout<<"str1 "<<str1<<endl;
+  cout<<"str2 "<<str2<<endl;
+}
+// return:
+// Before Copying
+// str1 Happy
+// str2 NewYear
+// After Copying
+// str1 NewYy
+// str2 NewYear
+
+/////5. strcat(str1, str2), copy the str2 behind the str1 coming to a new one string, without any effects on str2.
+int main(){
+  char str1[100]="Happy";
+  char str2[100]="NewYear";
+
+  cout<<"Before Copying"<<endl;
+  cout<<"str1 "<<str1<<endl;
+  cout<<"str2 "<<str2<<endl;
+
+  strcat(str1, str2);
+
+  cout<<"After Copying"<<endl;
+  cout<<"str1 "<<str1<<endl;
+  cout<<"str2 "<<str2<<endl;
+}
+//return:
+// Before Copying
+// str1 Happy
+// str2 NewYear
+// After Copying
+// str1 HappyNewYear
+// str2 NewYear
+
+
+////////////////////////////
+/////Print all Prefixes/////
+// set each result start index=0,and ending index is different use loop
+void printAllPrefixes(char str[]){
+  for (int end=0; end<strlen(str); end++){
+    for (int start=0; start<=end; start++){
+      cout<<str[start];
+    }
+    cout<<endl;
+  }
+}
+int main(){
+  char str[]="GoodMorning";
+  printAllPrefixes(str);
+  return 0;
+}
+// return:
+// G
+// Go
+// Goo
+// Good
+// GoodM
+// GoodMo
+// GoodMor
+// GoodMorn
+// GoodMorni
+// GoodMornin
+// GoodMorning
