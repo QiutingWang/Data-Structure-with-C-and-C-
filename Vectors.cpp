74c8fc0cd0030bf3fa7aa5cd2e2fcd5b86d01de8
QiutingWang <96046280+QiutingWang@users.noreply.github.com>
2023-01-13 19:31:07 +0800

    Update Vectors.cpp

--- Vectors.cpp
+++ Vectors.cpp
@@ -20,15 +20,28 @@ using namespace std;
 int main(){
   vector<int> v; //Statically: it will be deleted automatically, if it memory reach out of scope
   vector<int> *vp=new vector<int>(); //Dynamically: we have to delete it ourself, it cannot be deleted automatically
+  
+  ///Initialization: Create a vector of given size and value
+    //Syntax: vector<objectType> vectorName(size,value(optional));
+  vector<int> v2(10,-2); //all the values are initalized as -2 with size 10
+  for (int i=0;i<v2.size(); i++){
+    cout<<v2[i]<<endl;
+  }
+  // Sort the vector using .sort()
+  sort(v2.begin(), v2.end());
 
   for (int i=0; i<100;i++){
-    v.push_back(i+1);
+    cout<<"Capacity "<<v.capacity()<<endl; // .capacity(): give the current capacity of the vector. In this case, it doubly increases from 0,1...to 128 to contain 100 elements.
     cout<<v.size(); // it will increase linearly with i increasing from 1 to 100.
+    v.push_back(i+1);
   }
   for (int i=0;i<v.size();i++){
     cout<<v[i]<<endl;
   }      //it will return 1~100 
 
+/// Differences between capacity and size:
+// .size() gives how many elements are present
+// .capacity() gives how many elements you can insert inside the vector, the return type is double. It doubly increases. 0 1 2 4 4 8 8...
   /*
   v.push_back(10);  //.push_back() function: insert the element in the vector one by one
   v.push_back(20); 
@@ -98,3 +111,58 @@ int main(){
 // 99
 // 30
 // 77
+
+
+/////////////////////////////
+/////Spiral Order Matrix/////
+// the input is a matrix, then we need to traverse the matrix in spiral order顺时针方向
+///implementation
+// Four variables needed: startRow, endRow, startColumn, endColumn
+// Logic: startRow→endColumn→endRow→startColumn,then startRow++, endColumn--, endRow--, startColumn++ with for loop. 
+// add Constrain(stopping condition) with while: startColumn<=endColumn && startRow<=endRow;
+// Another stopping condition: count=0, as soon as the printed elements equal to m*n, stop the program
+///Example: LeetCode#54 Spiral Matrix https://leetcode.com/problems/spiral-matrix/
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> answer;
+        int m=matrix.size();
+        int n=matrix[0].size();
+
+        int startRow=0,endRow=m-1,startColumn=0,endColumn=n-1;
+        int count=0; 
+
+        while(startRow<=endRow && startColumn<=endColumn){
+            //print the startRow
+            for(int i=startColumn; i<=endColumn; i++){
+                answer.push_back(matrix[startRow][i]);
+                count++;
+            }
+            startRow++;
+            if(count==m*n) return answer;
+            //print the endColumn
+            for(int i=startRow; i<=endRow;i++){
+                answer.push_back(matrix[i][endColumn]);
+                count++;
+            }
+            endColumn--;
+            if(count==m*n) return answer;
+            //print the endRow
+            for(int i=endColumn;i>=startColumn;i--){
+                answer.push_back(matrix[endRow][i]);
+                count++;
+            }
+            endRow--;
+            if(count==m*n) return answer;
+            //print the startColumn
+            for(int i=endRow; i>=startRow; i--){
+                answer.push_back(matrix[i][startColumn]);
+                count++;
+            }
+            startColumn++;
+            if(count==m*n) return answer;
+        }
+        return answer;
+    }
+};
+//in this case: Space Complexity:O(1), Time Complexity:O(m*n)
