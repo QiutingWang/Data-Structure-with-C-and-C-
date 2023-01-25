4c2a0bb29422e7ad06898b6e98547b1276713969
QiutingWang <96046280+QiutingWang@users.noreply.github.com>
2023-01-25 12:42:56 +0800

    Update Merge Sort and Quick Sort.cpp

--- Merge Sort and Quick Sort.cpp
+++ Merge Sort and Quick Sort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-/////////////////////////////////
-/////Merge Sort Introduction/////
+////////////////////
+/////Merge Sort/////
 /// reference: https://www.geeksforgeeks.org/merge-sort/
 /// Procedure: Divide an array into sub-arrays→sort each sub-arrays→merge the sorted sub-arrays together
   // Division: continuously divide the array into two-equal halves,until it can not be cut any more. Namely, when length of sub-array==0 or 1.
@@ -81,3 +81,44 @@ int main(){
 // 10
 // 45
 // 92
+
+
+////////////////////
+/////Quick Sort/////
+// reference:https://www.geeksforgeeks.org/quick-sort/
+// pick an element as pivot and partitions the original array around the picked pivot
+// Different position selection of pivot: first element, last element, midpoint, random element
+  // Usually, we choose the last element as pivot.
+// partition: put x(pivot element) to a correct position in a sorted array, put elements<x before x without sorting, put elements>x after x without sorting, then repeatedly apply quick sort procedures to sub-arrays until all elements are sorted.
+int partition(int a[],int start,int end){
+  int i=start;
+  int pivot=a[end];
+  for(int j=start;j<=end-1;j++){ //traversing from left to right except the pivot element
+    if(a[j]<pivot){
+      swap(a[i],a[j]);
+      i++;
+    }
+  }
+  swap(a[i],a[end]); //put pivot in the correct position
+  return i; //this is the partition index p we need later
+}
+
+void quickSort(int a[],int start,int end){
+  if(start>=end){
+    return;
+  }
+  int p=partition(a,start,end); //the partition function will return the pivot index p
+  quickSort(a,start,p-1); //call the quickSort for unsorted two partitions
+  quickSort(a,p+1,end);
+}
+
+int main(){
+  int a[]={2,4,9,14,45,6};
+  quickSort(a,0,5); //call the function
+  for(int i=0;i<6;i++){ //print
+    cout<<a[i]<<" ";
+  }
+  return 0;
+}
+// return:
+// 2 4 6 9 14 45
