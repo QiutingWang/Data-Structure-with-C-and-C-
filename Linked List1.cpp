4933e0513951103a98fb42f79ba502e40224b139
QiutingWang <96046280+QiutingWang@users.noreply.github.com>
2023-02-15 22:13:42 +0800

    Update Linked List1.cpp

--- Linked List1.cpp
+++ Linked List1.cpp
@@ -186,3 +186,182 @@ int main(){
 // return:
 // 34-> 84-> 66-> 235-> 98-> 10-> NULL
 // T=O(N)
+
+
+////////////////
+/////Length/////
+class node{
+  public:
+    int data;
+    node *next; 
+    node(int data){
+      this->data=data; 
+      next=NULL;
+    }
+};
+// 🌟 Length function:
+int length(node *head){ //if we know the head of linked list, we know the whole linked list
+  int count=0;
+  while(head!=NULL){ //head exists
+    count++;
+    head=head->next; //move forward
+  }
+  return count;
+}
+
+node* takeInput(){ 
+  int data;
+  cin>>data;
+  node *head=NULL; 
+  node *tail=NULL; 
+  while(data!=-1){ 
+    node *n=new node(data); 
+    if(head==NULL){ 
+      head=n; 
+      tail=n;
+    }else{
+      tail->next=n; 
+      tail=n; 
+    }
+    cin>>data;
+  }
+  return head; 
+}
+
+int main(){
+  node *head=takeInput(); //only use insert at tail now
+  cout<<length(head)<<endl; //call length functions
+  return 0;
+}
+// return:
+// 10 98 235 66 84 34 -1
+// 6
+// T=O(N), owing to this method just traversing across the linked list.
+
+
+////////////////////////
+/////Print ith Node/////
+// first we need to reach at the specific index, using for/while loop moving ahead, then print the data
+// add this function below the length function
+void printIthNode(node *head, int i){
+  int n=length(head);
+  if(i<0 || i>n-1){
+    cout<<"-1"<<endl; //if the index is invalid
+    return;
+  }
+  //if the index is valid:
+  int count=1;
+  while(count<=i){
+    head=head->next; //moving ahead until reach the index we want
+    count++;
+  }
+  cout<<head->data<<endl; //as soon as reach the ith node, print the data
+}
+
+int main(){
+  node *head=takeInput();
+  printIthNode(head,3); //if we want to get the data at index 3
+  return 0;
+}
+// return:
+// 10 98 235 66 84 34 -1
+// 66
+// T=O(n+i), from time complexity, it is not perform well. If n>>i, the time complexity can be rewrite as T=O(n).
+
+// As a result, we can `optimize` the code:
+void printIthNode(node *head, int i){ //in this method, length function is not needed any more
+  if(i<0){
+    cout<<"-1"<<endl; //if the index is invalid
+    return;
+  }
+  int count=1;
+  while(count<=i && head!=NULL){  //if the index is valid
+    head=head->next; //moving ahead until reach the index we want
+    count++;
+  }
+  //Print:
+  if(head!=NULL){ //check whether head exists
+    cout<<head->data<<endl; //as soon as reach the ith node, print the data
+  }else{
+    cout<<"-1"<<endl; //the index is not valid
+  }
+}
+// the return is the same as the previous version
+// T=O(i). As for the worst case, i=n, T=O(n), which is the same as previous one. However, on average, the optimized solution is faster than previous one.
+
+
+///////////////////////////////////
+/////Insert Node @ith Position/////
+// if we want to insert an element at position i, we need to reach at position `i-1`, create a node with the specific value, create the connection with adjacent nodes, and the original link will be broken, shift the right side elements one unit
+// Two approaches:
+// 1. connect the new node with the next one→ connect the new node with previous one→original link broken(easier way)
+// 2.store the address of right side first remaining node as `temp`→connect the new node with previous one→ original link broken→the original right side remaining sub-linked list is lost(hence we need to storage the remaining nodes at first)→connect the new node with the first node of temp
+///Approach 1:
+node* insertAtIthPos(node *head,int i,int data){ //in this method, length function is not needed any more
+  if(i<0){ //invalid
+    return head;
+  }
+
+  if(i==0){ //insert at head
+    node* n=new node(data); //create a new node dynamically
+    n->next=head; //assign the address of original head in this node
+    head=n; //update head
+    return head;
+  }
+
+  node *copyHead=head;
+  int count=1;
+  while(count<=i-1 && head!=NULL){  //if the index is valid
+    head=head->next; //moving ahead until reach the index we want
+    count++;
+  }
+  //Print:
+  if(head!=NULL){ //check whether head exists
+    node *n=new node(data); //create a new node
+    n->next=head->next;
+    head->next=n;
+    return copyHead;
+  }
+  return copyHead; //if the value of index is invalid
+}
+
+int main(){
+  node *head=takeInput();
+  int i, data;
+  cin>>i>>data;
+  head=insertAtIthPos(head,i,data); //update the head
+  print(head); //use head function we have defined   
+  return 0;
+}
+// return:
+// 10 98 235 66 84 34 -1
+// 5 100
+// 10-> 98-> 235-> 66-> 84-> 100-> 34-> NULL
+
+// test another case, if we want to insert at head position
+// 10 98 235 66 84 34 -1
+// 0 100
+// 100-> 10-> 98-> 235-> 66-> 84-> 34-> NULL
+
+// test for invalid values, the return shows our linked list is unchanged
+// 10 98 235 66 84 34 -1
+// -4 99
+// 10-> 98-> 235-> 66-> 84-> 34-> NULL
+
+// test for insert at index=length position, using copyHead method
+// 10 98 235 66 84 34 -1
+// 6 99
+// 10-> 98-> 235-> 66-> 84-> 34-> 99-> NULL
+
+///Approach 2: only shows the changed part
+node* insertAtIthPos(node *head,int i,int data){ 
+  //Print:
+  if(head!=NULL){ //check whether head exists
+    node *n=new node(data); //create the new node
+    node *temp=head->next;//store the right side first remaining node address
+    head->next=n;
+    n->next=temp;
+    return copyHead;
+  }
+  return copyHead; //if the value of index is invalid
+}
