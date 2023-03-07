2022864f09a457b38d7de1259bcb969fc46c80d8
QiutingWang <96046280+QiutingWang@users.noreply.github.com>
2023-03-07 20:41:33 +0800

    Add files via upload

---
+++ Queues.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+////////////////////////////
+/////Queue Introduction/////
+// references: https://www.programiz.com/cpp-programming/queue
+// Abstract datatype with specific order to insert and delete elements
+// FIFO: elements are added first that will be removed first.
+// methods:
+  // push(): inserts an element at the back
+  // pop():removes an element from the `front` of the queue
+  // front(): returns the first element 
+  // back():returns the last element 
+  // size()
+  // empty():returns true if the queue is empty
+// Implement: Array(& Dynamic Array) or Linked List, inbuilt Queue
+
+
+////////////////////////////////////////
+/////Queue Using Array Introduction/////
+
+
+
+
