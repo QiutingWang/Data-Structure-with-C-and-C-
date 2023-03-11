cb2ffb5b02697d01e69b503460f4ac4faa678739
QiutingWang <96046280+QiutingWang@users.noreply.github.com>
2023-03-11 18:54:55 +0800

    Update Queues.cpp

--- Queues.cpp
+++ Queues.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <queue>
 using namespace std;
 ////////////////////////////
 /////Queue Introduction/////
@@ -178,3 +177,192 @@ class queue{
 // 0
 // 66
 // 4
+
+
+//////////////////////////////////
+/////Queue Using Linked List /////
+///push()
+// We have head pointer only, we need to iterate over the linked list to reach the last node, and make the new connection. As consequences, T=O(N)
+// Additionally, we can optimize our method to obtain T=O(1) by maintaining tail pointer as well.
+template<typename T>
+class node{
+  public:
+    T data;
+    node* next;
+    node(T data){
+      this->data=data;
+      next=NULL;
+    }
+};
+
+template<typename T>
+class queue{
+  node<T>* head;
+  node<T>* tail;
+  int size;
+
+  public:
+    queue(){ //constructor
+      head=NULL;
+      tail=NULL;
+      size=0;
+    }
+    int getSize(){
+      return size;
+    }
+    bool isEmpty(){
+      return size==0;
+    }
+    void push(T element){
+      node<T>* n=new node<T>(element); //create a new node
+      if(head==NULL){ //check empty
+        head=n; 
+        tail=n;
+      }else{
+        tail->next=n; //move ahead and set the newly created node
+        tail=n; //update
+      }
+      size++;
+    }
+    T front(){
+      if(isEmpty()){ //check empty
+        return 0;
+      }
+      return head->data;
+    }
+    void pop(){
+      if(head==NULL){ //check empty
+        return;
+      }
+      node<T>* temp=head; //use a temporary pointer
+      head=head->next;//move forward
+      temp->next=NULL; //isolation
+      delete temp;
+      size--;
+    }
+};
+
+int main(){
+  queue<int> q;
+  q.push(11);
+  q.push(22);
+  q.push(33);
+  q.push(44);
+  q.push(55);
+  q.push(66);
+  q.push(77);
+  cout<<q.front()<<endl;
+  q.pop();
+  q.pop();
+  q.pop();
+  cout<<q.front()<<endl;
+  cout<<q.getSize()<<endl;
+  cout<<q.isEmpty()<<endl;
+  q.push(88);
+  q.push(99);
+  q.pop();
+  q.pop();
+  cout<<q.front()<<endl;
+  cout<<q.getSize()<<endl;
+
+  while(!q.isEmpty()){
+    cout<<q.front()<<endl;
+    q.pop();
+  }
+  cout<<q.getSize()<<endl;
+  cout<<q.isEmpty()<<endl;
+  return 0;
+}
+// return
+// 11
+// 44
+// 4
+// 0
+// 66
+// 4
+// 66
+// 77
+// 88
+// 99
+// 0
+// 1
+
+
+///////////////////////
+/////Inbuilt Queue/////
+// we do not need to define class queue manually
+#include <queue>
+
+int main(){
+  queue<int> q;
+  q.push(11);
+  q.push(22);
+  q.push(33);
+  q.push(44);
+  q.push(55);
+  cout<<q.front()<<endl;
+  q.pop();
+  q.pop();
+  cout<<q.front()<<endl;
+  cout<<q.size()<<endl;
+  q.push(66);
+  q.push(77);
+  q.pop();
+  cout<<q.front()<<endl;
+  cout<<q.size()<<endl;
+
+  while(!q.empty()){
+    cout<<q.front()<<endl;
+    q.pop();
+  }
+  cout<<q.size()<<endl;
+  return 0;
+}
+// return
+// 11
+// 33
+// 3
+// 44
+// 4
+// 44
+// 55
+// 66
+// 77
+// 0
+
+// Always use the inbuilt queue thanks to its simplicity, without the needs of using linked list or array to build the extra define class.
+
+
+/////////////////////////
+/////Reverse a Queue/////
+#include <stack>
+// push the queue elements in stack, then take them out of stack to reform a new queue with an reverse order
+// Pass by value VS pass by reference. https://www.educative.io/answers/pass-by-value-vs-pass-by-reference
+void reverse(queue<int> &q){ //pass q by reference, not by value. If we use call by value, we only return function q in this reverse function, which is different from what we will get in main funciton about q
+  stack<int> s;
+  while(!q.empty()){
+    s.push(q.front());
+    q.pop();
+  }
+  // q is empty, stack is full. This time we push the elements from stack to queue
+  while(!s.empty()){
+    q.push(s.top());
+    s.pop();
+  }
+}
+
+int main(){
+  queue<int> q;
+  for(int i=1;i<=5;i++){
+    q.push(i);
+  }
+  reverse(q);
+  // print
+  while(!q.empty()){
+    cout<<q.front()<<" ";
+    q.pop();
+  }
+  return 0;
+}
+// return
+// 5 4 3 2 1
