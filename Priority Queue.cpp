7af6aa6c398e3d868a24f6286ed3c7fcd4359515
QiutingWang <96046280+QiutingWang@users.noreply.github.com>
2023-04-28 14:39:27 +0800

    Update Priority Queue.cpp

--- Priority Queue.cpp
+++ Priority Queue.cpp
@@ -89,11 +89,11 @@ class PriorityQueue{
     void insert(int element){
       pq.push_back(element); //put it in the last position, it is a CBT, but cannot promise it obeys heap order property yet
       //apply the formula
-      int ChildIndex=pd.size()-1;
+      int ChildIndex=pq.size()-1;
       while(ChildIndex>0){ //child index begins from 1
         int ParentIndex=(ChildIndex-1)/2;
         //comparison and swap
-        if(pq[ChildIndex]<pd[ParentIndex]){
+        if(pq[ChildIndex]<pq[ParentIndex]){
           swap(pq[ChildIndex],pq[ParentIndex]);
           ChildIndex=ParentIndex; //update the child index
           }else{
@@ -152,5 +152,190 @@ int main(){
   }
   return 0;
 }
+// return
+// 7
+// 5
+// 5 10 14 32 55 96 126
+// Time complexity=O(NlogN)
+// Space complexity=O(N)
 
 
+///////////////////////////
+/////Inplace Heap Sort/////
+/////Optimization: to make space complexity becomes O(1)
+// Solution: convert the input array into minimum heap
+// component of min heap algorithm mentioned above: 1. build minimum heap;  2. call the remove min function N times
+// Procedure:
+  // Assumption: the first element is the part of heap, the remaining n-1 elements are unsorted array
+  // once take next element of array into heap part, we need to ensure it obeys heap order property, comparison and consider whether we need swap operation
+  // As a result, all of the elements are sorted expect the original head element whose current position at the last.
+  // swap the first element(A) and last element(B), and assume A has been removed(but actually not). Use `size` to represent it
+  // swap the remaining part of heap to make sure it obeys heap order property
+  // iterate the last 2 steps with N times. Namely, as soon as the child index=0, stop criteria satisfied.
+  // As consequences, the array is sorted in decreasing order. While, if we use max priority queue, the array is sorted in ascending order.
+// T=O(NlogN), S=O(1)
+void inplaceHeapSort(int *pq, int n){
+  // build heap in input array
+  for(int i=1; i<n; i++){ //assume the first element at index 0 is already in heap
+    int ChildIndex=i;
+      while(ChildIndex>0){ 
+        int ParentIndex=(ChildIndex-1)/2;
+        if(pq[ChildIndex]<pq[ParentIndex]){
+          swap(pq[ChildIndex],pq[ParentIndex]);
+          ChildIndex=ParentIndex; 
+          }else{
+            break;
+          }
+      }    
+  }
+  //call the remove min function for n times
+  int size=n;
+  while(size>0){
+  swap(pq[0],pq[size-1]);
+  size--; //assume the element is removed but actually not. We use size to represent that.
+  // Down-heapify
+  int ParentIndex=0;
+      while(true){
+        int leftChildIndex=2*ParentIndex+1;
+        int rightChildIndex=2*ParentIndex+2;
+        int minIndex=ParentIndex;
+
+        if(leftChildIndex<size && pq[leftChildIndex]<pq[minIndex]){
+          minIndex=leftChildIndex; 
+        }
+        if(rightChildIndex<size && pq[rightChildIndex]<pq[minIndex]){
+          minIndex=rightChildIndex;
+        }
+        if(minIndex==ParentIndex){
+          break;
+        }
+        swap(pq[ParentIndex],pq[minIndex]);
+        ParentIndex=minIndex;
+      }
+  }
+}
+
+int main(){
+  int pq[]={14, 126, 55, 96, 32, 10, 5};
+  inplaceHeapSort(pq, 7);
+  for(int i=0;i<7;i++){
+    cout<<pq[i]<<" ";
+  }
+  return 0;
+}
+// return
+// 126 96 55 32 14 10 5
+
+
+////////////////////////////////
+/////Inbuilt Priority Queue/////
+// STL Format: priority_queue<value_type> variable_name
+#include <queue>
+int main(){
+  priority_queue<int> pq;
+  pq.push(14);
+  pq.push(126);
+  pq.push(55);
+  pq.push(96);
+  pq.push(32);
+  pq.push(10);
+  pq.push(5);
+  cout<<"Size: "<<pq.size()<<endl;
+  cout<<"Top: "<<pq.top()<<endl; //return the root element
+  while(!pq.empty()){
+    cout<<pq.top()<<endl;
+    pq.pop(); //print the maximum element then delete it
+  }
+  return 0;
+}
+// return
+// Size: 7
+// Top: 126
+// 126
+// 96
+// 55
+// 32
+// 14
+// 10
+// 5
+// other STL functions: swap(), emplace(), value_type
+
+
+////////////////////////
+/////K Sorted Array/////
+// Rule: the maximum distance one element can move left or right for being sorted is up to `K-1` unit
+// inputs we need to give: array and k
+// Sorting:
+  // Method1: blindly and simply use inbuilt sort() function. T=O(NlogN)
+  // Method2: use Maximum priority queue
+    // Process: the container contains k elements at most composing a Priority Queue, we pick up the maximum one as the sorted part with newly arranged index literately. 
+void KSortedArray(int *input, int n, int k){
+  priority_queue<int> pq;
+  for(int i=0;i<k;i++){
+    pq.push(input[i]); //insert the first k elements
+  }
+  int start=0; //set a pointer
+  for(int i=k;i<n;i++){
+    input[start]=pq.top(); //get the maximum element
+    pq.pop(); //remove it
+    start++; //move ahead
+    pq.push(input[i]); //insert the ith element into the priority queue
+  }
+  while(!pq.empty()){
+    input[start]=pq.top();
+    pq.pop();
+    start++;
+  }
+}
+
+int main(){
+  int input[]={14, 126, 55, 96, 32, 10, 5};
+  int k=3;
+  KSortedArray(input, 7, k);
+  for(int i=0;i<7;i++){
+    cout<<input[i]<<" ";
+  }
+  return 0;
+}
+// return
+// 126 96 55 32 14 10 5
+// T=O(Nlogk), if the k is very small, T≈O(N), S=O(k)≈O(1)
+
+
+////////////////////////////
+/////K Smallest Element/////
+// procedure: 
+  // push the first k elements to set up a priority queue
+  // comparison: 
+    // if the next element < the largest element in priority queue-->swap
+    // if the next element > the largest element in priority queue-->do nothing
+  // iteration the comparison step 
+  // reverse the final results of the k-element priority queue is what we want
+void KSmallest(int *a, int n, int k){
+  priority_queue<int> pq;
+  //push the first k elements
+  for(int i=0;i<k;i++){
+    pq.push(a[i]);
+  }
+  //comparison
+  for(int i=k;i<n;i++){
+    if(a[i]<pq.top()){
+      pq.pop();
+      pq.push(a[i]);
+    }
+  }
+  //get answer
+  while(!pq.empty()){
+    cout<<pq.top()<<" ";
+    pq.pop();
+  }
+}
+
+int main(){
+  int a[]={14, 126, 55, 96, 32, 10, 5};
+  KSmallest(a,7,3);
+  return 0;
+}
+// return
+// 14 10 5
+// T≈O(N), S=O(k)≈O(1)
